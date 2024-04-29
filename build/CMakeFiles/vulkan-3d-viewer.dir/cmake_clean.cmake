643f0c14ed99c9163cbfb72952cccdf0a9ec139e
Patrick <patrickprodrigues@hotmail.com>
2024-04-29 08:01:25 -0300

    basic vulkan setup using cmake

---
+++ build/CMakeFiles/vulkan-3d-viewer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vulkan-3d-viewer.dir/source/main.cpp.o"
+  "CMakeFiles/vulkan-3d-viewer.dir/source/main.cpp.o.d"
+  "vulkan-3d-viewer"
+  "vulkan-3d-viewer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vulkan-3d-viewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
